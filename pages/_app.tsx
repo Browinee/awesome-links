f6109d3f68b57f8bd105b7c4922d20258f0a1595
justin <>
2021-11-02 10:57:23 +0800

    feat: add @apollo/client and query the link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/tailwind.css';
 import Layout from '../components/Layout';
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({Component, pageProps}) {
+    return (
+        <ApolloProvider client={apolloClient}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ApolloProvider>
+
+    );
 }
 
 export default MyApp;
